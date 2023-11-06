5a79705741f40255f2b3102142a4960bcdb9f92a
Jus Ferrell <j1ferrel@ucsd.edu>
2023-11-06 05:27:55 -0800

    required html structure, modified styling such as light/dark theme, early stages of js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function() {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // function? How can DOM traversal be used to get the 'hour-x' id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
@@ -20,4 +20,33 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+
+    //variables for toggling color theme
+    var isLight = true;
+    var lightThemeEl = $('.light');
+    var themeToggleEl = $('#theme-toggle');
+
+    //sets calendar date 
+    function setDate() {
+        var monthYear = dayjs();
+        var monthDay = dayjs();
+
+        $('.monthYear').text((monthYear).format('MMMM YYYY'));
+        $('.monthDay').text((monthDay).format('DDDD, MMM D'));
+    }
+
+    //click event toggles color theme based on current state
+    themeToggleEl.on('click', function() {
+        if (isLight) {
+            lightThemeEl.attr('class', 'dark');
+            isLight = !isLight;
+            console.log(isLight);
+        } else {
+            lightThemeEl.attr('class', 'light');
+            isLight = !isLight;
+            console.log(isLight);
+        }
+    });
+
+    setDate();
 });
